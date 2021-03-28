f2e2ea3a4fe18d43bd68a4435bccebd15aa0f687
stancristina <stancristina46@gmail.com>
2021-03-28 21:57:06 +0300

    "edit home page, added setup if a user watched an entire lesson"

--- src/main/webapp/app/entities/course/course-detail.component.ts
+++ src/main/webapp/app/entities/course/course-detail.component.ts
@@ -6,6 +6,9 @@ import { IChapter } from '../../shared/model/chapter.model';
 import { HttpResponse } from '@angular/common/http';
 import { CourseService } from './course.service';
 import { ILesson } from '../../shared/model/lesson.model';
+import { IUserDetailsLesson } from '../../shared/model/user-details-lesson.model';
+import { IUserDetailsChapter } from '../../shared/model/user-details-chapter.model';
+import { IUserDetailsCourse } from '../../shared/model/user-details-course.model';
 
 @Component({
   selector: 'jhi-course-detail',
@@ -17,6 +20,10 @@ export class CourseDetailComponent implements OnInit {
   activeIds: string[] = [];
   activeLesson: ILesson | undefined;
   activeChapter: IChapter | undefined;
+
+  userDetailsLessons: IUserDetailsLesson[] | undefined;
+  userDetailsChapters: IUserDetailsChapter[] | undefined;
+  userDetailsCourse: IUserDetailsCourse | undefined;
 
   constructor(protected activatedRoute: ActivatedRoute, protected courseService: CourseService) {}
 
@@ -47,12 +54,27 @@ export class CourseDetailComponent implements OnInit {
       "<source src='../../../content/videos/" + selectedLesson.url + "' type='video/mp4'> Error, no video loaded";
     mainVideoContainer.load();
     mainVideoContainer.play();
+
     this.activeLesson = selectedLesson;
     for (let i = 0; i < this.chapters!.length; i++) {
       if (this.chapters![i] !== undefined && this.chapters![i].id === this.activeLesson.chapterId) {
         this.activeChapter = this.chapters![i];
       }
     }
+
+    mainVideoContainer.addEventListener('ended', this.onLessonEnded);
+  }
+
+  sliceLessonTitle(lessonTitle: String): String {
+    let toReturn = lessonTitle.slice(0, 40);
+    if (lessonTitle.length > 40) {
+      toReturn += '...';
+    }
+    return toReturn;
+  }
+
+  onLessonEnded(): any {
+    alert('The lesson has been ended');
   }
 
   ngOnInit(): void {
