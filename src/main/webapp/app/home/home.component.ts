4ecb00c08673a66d2d11e5f9571c07625479f8fb
stancristina <stancristina46@gmail.com>
2021-05-26 19:08:54 +0300

    added background

--- src/main/webapp/app/home/home.component.ts
+++ src/main/webapp/app/home/home.component.ts
@@ -14,7 +14,14 @@ export class HomeComponent implements OnInit, OnDestroy {
   account: Account | null = null;
   authSubscription?: Subscription;
 
-  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {}
+  constructor(private accountService: AccountService, private loginModalService: LoginModalService) {
+    const card = document.getElementsByClassName('jh-card')[0] as HTMLElement;
+    card.classList.remove('jh-card');
+    card.classList.add('jh-card-custom');
+
+    const footer = document.getElementsByTagName('jhi-footer')[0] as HTMLElement;
+    footer.style.visibility = 'hidden';
+  }
 
   ngOnInit(): void {
     this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
@@ -31,6 +38,17 @@ export class HomeComponent implements OnInit, OnDestroy {
   ngOnDestroy(): void {
     if (this.authSubscription) {
       this.authSubscription.unsubscribe();
+    }
+
+    const card = document.getElementsByClassName('jh-card-custom')[0] as HTMLElement;
+    if (card !== undefined) {
+      card.classList.add('jh-card');
+      card.classList.remove('jh-card-custom');
+    }
+
+    const footer = document.getElementsByTagName('jhi-footer')[0] as HTMLElement;
+    if (footer !== undefined) {
+      footer.style.visibility = 'visible';
     }
   }
 }
